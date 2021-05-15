6b6754d73e002c332ca60e0e9e7b93f17e50034e
Garrett Bauer <bauer.garrett9@gmail.com>
2021-05-15 01:52:56 -0700

    Started messing around with flask-sqlalchemy

--- homebase.py
+++ homebase.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from config import *
 import requests
 
+# SETUP
+
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# MODEL
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# VIEWS
 
 @app.route('/')
 def index():
@@ -16,12 +34,19 @@ def projects():
 
     repos = requests.get(url, auth=(USERNAME,TOKEN)).json()
 
-    projects = [(repo["name"], repo["description"], repo["html_url"]) for repo in repos]
+    projects = [(repo["name"], repo["description"], repo["html_url"]) if repo["description"] else (repo["name"], "No README available", repo["html_url"]) for repo in repos]
     return render_template("projects.html", projects=projects)
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
+@app.route('/blog/')
+@app.route('/blog/<entry>')
+def blog(entry=None):
+    users = User.query.all()
+    return render_template('blog.html', users=users)
+
+
 
 if __name__ == "__main__":
     app.run()
+
+# TODO: Refactor into multiple files to separate model from views
+# TODO: Create admin page

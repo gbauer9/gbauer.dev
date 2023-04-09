a4d39de488e0b38aef1155f5fa3bcd1763611f88
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-09 01:01:44 -0700

    small change

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,9 +3,8 @@ import { type } from "@camwiegert/typical";
 
 const Home: React.FC = () => {
     const typicalRef = useRef(null);
-    const subtitleRef = useRef(null);
 
-    const steps = [2500, "G", 100, "Ga", 100, "Gar", 100, "Garr", 100, "Garre", 100, "Garret", 100, "Garrett", 100, "Garrett ", 100, "Garrett B", 100, "Garrett Ba", 100, "Garrett Bau", 100, "Garrett Baue", 100, "Garrett Bauer"];
+    const steps = [2500, "Garrett Bauer"]
 
     useEffect(() => {
         type(typicalRef.current, ...steps);

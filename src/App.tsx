213c47759e9393ca35e2d879874bd6892ab4b67f
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-12 20:26:06 -0700

    use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
@@ -12,12 +12,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <Routes>
+      <HashRouter>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<NoMatch />} />
-      </Routes>
+      </HashRouter>
       <Footer />
     </>
   );

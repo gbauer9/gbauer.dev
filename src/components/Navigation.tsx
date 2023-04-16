a6937e1de19be52ac49aa28602c76963ab869f57
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-15 18:53:28 -0700

    remove import

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import { NavLink } from 'react-router-dom';

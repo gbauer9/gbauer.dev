a39fc32373cee4dc8426b0c90b34c550fa330998
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-16 20:58:10 -0700

    cleanup

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -6,7 +6,10 @@ const About: React.FC = () => {
     <div className="text-left">
       <img src={profile} className="rounded mx-auto my-5 d-block" id="profile-pic" alt="Profile"/>
       <p>Hi, my name's Garrett and I'm a Software Engineer with 3.5+ years of experience from Portland, Oregon.
-         I'm super passionate about backend development, solving problems, and working with talented, cool people. I'm always looking to challenge myself and learn new things! If you'd like to work with me, feel free to reach out via <a href="mailto:bauer.garrett9@gmail.com">email</a> or <a href="https://www.linkedin.com/in/gbauer9/">LinkedIn</a>.</p>
+         I'm super passionate about backend development, solving problems, and working with talented, cool people. 
+         I'm always looking to challenge myself and learn new things! 
+         If you'd like to work with me, feel free to reach out via <a href="mailto:bauer.garrett9@gmail.com">email</a> or 
+         <a href="https://www.linkedin.com/in/gbauer9/"> LinkedIn</a>.</p>
   </div>
   );
 };

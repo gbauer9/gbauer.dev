705c7fb91889b398a2a0d8b143b8b6ddb1fe07bc
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-16 17:06:18 -0700

    change links to icons

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -4,7 +4,7 @@ import profile from '../images/profile.jpg'
 const About: React.FC = () => {
   return (
     <div className="text-left">
-      <img src={profile} className="rounded mx-auto my-5 d-block" id="profile-pic"/>
+      <img src={profile} className="rounded mx-auto my-5 d-block" id="profile-pic" alt="Profile"/>
       <p>Hi, my name's Garrett and I'm a Software Engineer with 3.5+ years of experience from Portland, Oregon.
          I'm super passionate about backend development, solving problems, and working with talented, cool people. I'm always looking to challenge myself and learn new things! If you'd like to work with me, feel free to reach out via <a href="mailto:bauer.garrett9@gmail.com">email</a> or <a href="https://www.linkedin.com/in/gbauer9/">LinkedIn</a>.</p>
   </div>

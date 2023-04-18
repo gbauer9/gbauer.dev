38c77d3b80a715edb4c78c33f643f39d5a5a5936
Garrett Bauer <bauer.garrett9@gmail.com>
2023-04-18 00:18:34 -0700

    change yoe to be automatic

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import profile from '../images/profile.jpg'
+import moment from 'moment';
 
 const About: React.FC = () => {
+
+  const getMonthsExperience = () => {
+    const start = moment("2019-07-07");
+    const now = moment();
+
+    return now.diff(start, "months");
+  }
+
   return (
     <div className="text-left">
       <img src={profile} className="rounded mx-auto my-5 d-block" id="profile-pic" alt="Profile"/>
-      <p>Hi, my name's Garrett and I'm a Software Engineer with 3.5+ years of experience from Portland, Oregon.
+      <p>Hi, my name's Garrett and I'm a Software Engineer with {getMonthsExperience() % 12 < 6 ? Math.floor(getMonthsExperience() / 12) : Math.floor(getMonthsExperience() / 12) + 1} years of experience from Portland, Oregon.
          I'm super passionate about backend development, solving problems, and working with talented, cool people. 
          I'm always looking to challenge myself and learn new things! 
          If you'd like to work with me, feel free to reach out via <a href="mailto:bauer.garrett9@gmail.com">email</a> or 
